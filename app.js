77e1b22c704de2551489b2f85ee057f5597971cd
Daniel Goehring <danielgoehring73@gmail.com>
2023-05-06 19:31:54 -0400

    enabled cards to be flipped on mobile devices

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+const flipCard = document.getElementById('flip-card');
+
+    // Add touch event listener for mobile devices
+    flipCard.addEventListener('touchstart', function () {
+        // Toggle the 'flipped' class on the flip-card-inner element
+        this.querySelector('.flip-card-inner').classList.toggle('flipped');
+    });
