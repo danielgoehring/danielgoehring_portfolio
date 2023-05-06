f6ca87362a6f5a08e29286e9722ae8bd306cfab4
Daniel Goehring <danielgoehring73@gmail.com>
2023-05-06 19:59:08 -0400

    edited mobile feature

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 
-const flipCard = document.getElementById('flip-card');
+// Get all the flip-card elements
+const flipCards = document.getElementsByClassName('flip-card-item');
 
-    // Add touch event listener for mobile devices
-    flipCard.addEventListener('touchstart', function () {
+// Add touch event listeners for mobile devices
+for (let i = 0; i < flipCards.length; i++) {
+    flipCards[i].addEventListener('touchstart', function () {
         // Toggle the 'flipped' class on the flip-card-inner element
         this.querySelector('.flip-card-inner').classList.toggle('flipped');
     });
+}
